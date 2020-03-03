fbd6b7e1c8999fae7b37b2d77cef876a213ab57f
Ekaterina <nataveres@gmail.com>
2020-03-03 15:56:17 +0100

    First part of exercises

--- 1-isolate/challenge-1.js
+++ 1-isolate/challenge-1.js
@@ -1,20 +1,24 @@
-const evaluate = require('../evaluate');
+const evaluate = require("../evaluate");
 
 const earlyReturn1Tests = [
-  { name: 'Test 1', args: ['', ''], expected: 'string' },
-  { name: 'Test 2', args: [false, ''], expected: 'hi!' },
-  { name: 'Test 3', args: [false, true], expected: 'boolean' },
-  { name: 'Test 4', args: ['hi!', true], expected: 'string' },
-  { name: 'Test 5', args: [2, 3], expected: 'hi!' },
+  { name: "Test 1", args: ["", ""], expected: "string" }, //
+  { name: "Test 2", args: [false, ""], expected: "hi!" },
+  { name: "Test 3", args: [false, true], expected: "boolean" }, //
+  { name: "Test 4", args: ["hi!", true], expected: "string" }, //
+  { name: "Test 5", args: [2, 3], expected: "hi!" }
 ];
 
 function earlyReturn1(a, b) {
   // write early returns to pass the asserts and the tests
+  if (typeof a === "boolean" && typeof b === "boolean") {
+    return "boolean"; //
+  }
+  if (typeof a === "string") return "string"; //
 
-  console.assert(typeof a !== 'string', 'a should not be a string');
-  console.assert(typeof b !== 'boolean', 'b should not be a boolean');
+  console.assert(typeof a !== "string", "a should not be a string");
+  console.assert(typeof b !== "boolean", "b should not be a boolean");
 
-  return 'hi!';
+  return "hi!";
 }
 
 evaluate(earlyReturn1, earlyReturn1Tests);

5c1ebb9ea714b6a3bb5094562259ffb524cec720
Ekaterina <nataveres@gmail.com>
2020-03-03 16:51:56 +0100

    Second part of exercises

--- 2-integrate/2-return-as-numbers.js
+++ 2-integrate/2-return-as-numbers.js
@@ -1,26 +1,34 @@
-const evaluate = require('../evaluate');
+const evaluate = require("../evaluate");
 
-const thingsToNumber1 = ['.', '1', '2', ':'];
-const thingsToNumber2 = ['1', 'two', 'three', '10'];
-const thingsToNumber3 = ['one', '2', '', 'NaN'];
-const thingsToNumber4 = ['.', 1, 2, NaN];
+const thingsToNumber1 = [".", "1", "2", ":"];
+const thingsToNumber2 = ["1", "two", "three", "10"];
+const thingsToNumber3 = ["one", "2", "", "NaN"];
+const thingsToNumber4 = [".", 1, 2, NaN];
 
-const oddsToNumber = ['1', '3', '5'];
-const evensToNumber = ['2', '4', '6'];
+const oddsToNumber = ["1", "3", "5"];
+const evensToNumber = ["2", "4", "6"];
 
 const returnAsNumbersTests = [
-  { name: 'Test 1', args: [thingsToNumber1], expected: [1, 2] },
-  { name: 'Test 2', args: [thingsToNumber2], expected: [1, 10] },
-  { name: 'Test 3', args: [thingsToNumber3], expected: [2, 0] },
-  { name: 'Test 4', args: [thingsToNumber4], expected: null },
-  { name: 'Test 5', args: [[1, 2, 3]], expected: null },
-  { name: 'Test 6', args: [oddsToNumber], expected: [1, 3, 5] },
-  { name: 'Test 7', args: [evensToNumber], expected: [2, 4, 6] },
+  { name: "Test 1", args: [thingsToNumber1], expected: [1, 2] },
+  { name: "Test 2", args: [thingsToNumber2], expected: [1, 10] },
+  { name: "Test 3", args: [thingsToNumber3], expected: [2, 0] },
+  { name: "Test 4", args: [thingsToNumber4], expected: null },
+  { name: "Test 5", args: [[1, 2, 3]], expected: null },
+  { name: "Test 6", args: [oddsToNumber], expected: [1, 3, 5] },
+  { name: "Test 7", args: [evensToNumber], expected: [2, 4, 6] }
 ];
 
-const returnAsNumbers = (arr) => {
+const returnAsNumbers = arr => {
   // cast all non-NaNy strings to numbers and return the new array
   // Hint: early return condition --> array contains any non-strings
+  if (arr.some(el => typeof el !== "string")) return null;
+  let rez = [];
+  arr.forEach(el => {
+    if (!isNaN(el)) {
+      rez.push(+el);
+    }
+  });
+  return rez;
 };
 
 evaluate(returnAsNumbers, returnAsNumbersTests);
